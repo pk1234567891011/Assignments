4b097b2435c74fc8ae9f0316917b175373cb9577
Priyanka <priyankakumari.h@somaiya.edu>
2019-11-06 13:48:15 +0530

    HTML AND CSS

---
+++ Html_css_assignment/php.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,"Employee");
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
+
